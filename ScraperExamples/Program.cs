54791ada86c9f98aa7a5b67d429f9b2c377bbefe
jialang zhang <jialangzhang@jialangs-MacBook-Pro.local>
2017-11-01 23:42:21 -0400

    updated example project

--- ScraperExamples/Program.cs
+++ ScraperExamples/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-
+using System.Linq;
+using SharpContentScraper;
 namespace ScraperExamples
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ParseHuffingtonPost();
         }
+        static void ParseHuffingtonPost()
+        {
+            var news =  Scraper.Get("https://www.huffingtonpost.com")
+                   .GetElements(".bn-card")
+                   .MapToObjects<News>(
+                       (new Mapper())
+                        .MapAttr(".bn-card-headline","href","Url")
+                        .MapText(".bn-card-headline", "Title")
+                                  ).ToList();
+
+            foreach(var n in news)
+            {
+                Console.WriteLine(n.Title + " " + n.Url);
+                
+            }
+        }
+
+    }
+    public class News
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
     }
 }
